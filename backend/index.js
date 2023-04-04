b91f13e88daeab53c1b8923519b3c859c59421c5
tungle4922 <113108918+tungle4922@users.noreply.github.com>
2023-04-04 23:28:32 +0700

    fix be

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+//use express.json() to get data into json format
+app.use(express.json());
 // use cors
 app.use(cors());
 
